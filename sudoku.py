a24868824004edc412dfdd24e3a67ede40cd6577
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2021-02-20 16:03:47 +0100

    add function "solve"

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,6 @@
 #function to check if it is possible to insert a number in square
 def isPossible(sudoku, row, col, number):
-
+    
     #checking if in row there is already no number that we want to insert
     for j in range(len(sudoku)):
         if sudoku[row][j] == number:
@@ -19,17 +19,56 @@ def isPossible(sudoku, row, col, number):
 
     return True            
 
-sudoku = [
-[2,0,0,3,6,0,0,0,0],
-[0,0,0,0,0,0,0,3,0],
-[0,0,3,2,4,0,0,9,0],
-[0,2,7,0,0,0,0,1,0],
-[0,3,1,6,0,0,9,0,0],
-[6,0,9,8,1,0,2,0,7],
-[9,6,2,4,0,0,3,0,0],
-[1,0,5,0,3,0,4,2,0],
-[3,7,4,0,8,2,5,6,0]
+import time
+def solve(sudoku,row=0, col=0):
+    if col > 8 or row > 8:
+        return
 
+    if row == 8 and col == 8:
+        temp_alt = True
+        for x in sudoku:
+            if x.count(0) != 0:
+                temp_alt = False
+                break
+        if temp_alt:
+            for x in sudoku:
+                print(x)
+            exit(0)
+
+    if sudoku[row][col] == 0:
+        for number in range(1, 10):
+            temp = False
+            if isPossible(sudoku, row, col, number):
+                temp = True
+                sudoku[row][col] = number
+                
+                
+                if col < 8:
+                    solve(sudoku, row, col + 1)
+                elif col == 8 and row < 8:
+                    solve(sudoku, row + 1, 0)
+
+        else:
+            sudoku[row][col] = 0
+            return
+            
+    else:
+        if col < 8:
+            solve(sudoku, row, col + 1)
+        elif col == 8 and row < 8:
+            solve(sudoku, row + 1, 0)
+
+
+sudoku = [
+[8,0,4,0,0,5,1,0,6],
+[5,0,6,7,0,8,0,0,0],
+[0,9,0,0,0,6,8,7,5],
+[0,6,1,0,8,0,9,0,2],
+[0,0,0,2,9,0,0,0,7],
+[0,0,2,0,4,0,0,0,1],
+[0,3,0,1,0,0,0,6,0],
+[0,2,0,0,0,0,7,3,0],
+[0,8,0,3,0,2,0,0,4]
 ]
-for x in sudoku:
-    print(x)
+
+solve(sudoku)
